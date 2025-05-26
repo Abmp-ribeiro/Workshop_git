6fa1c949f9c748301d1e2ae35cd31261aec8976e
Abmpr <amandamatosribeiro@gmail.com>
2025-05-26 09:52:36 -0400

    Added a mission

---
+++ Alunos/missao.py
@@ -0,0 +1,4 @@
+''' Cada um de vocês deve criar sua própria branch e adicionar a pasta Aluno um arquivo
+python que de print em um texto sobre algo bom da semana passada. Exemplo:'''
+
+print("Na semana passada em recebia a nota de uma prova que fui muito bem")
